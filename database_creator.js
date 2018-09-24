b683f737ec6a89c742c0b144fb435080c1d7d47f
Sjors Suijkerbuijk <sjorssuijkerbuijk@gmail.com>
2018-09-24 11:38:17 +0200

    database creation with sequelize

---
+++ database_creator.js
@@ -0,0 +1,59 @@
+// load environment variables
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+//CLI actions
+//dropdb blog-application
+//createdb -h localhost -p 5432 -U postgres blog-application
+
+//Create connection
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    timestamps: false
+  }
+})
+
+//Setting up the structure of the database
+const Users = sequelize.define('users', {
+  user_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+	user_name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+const Posts = sequelize.define('posts', {
+  post_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+	post_title: Sequelize.STRING,
+  post_body: Sequelize.STRING
+});
+
+const Comments = sequelize.define('comments', {
+  comment_id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+	comment_body: Sequelize.STRING
+});
+
+//Setting relations of the database
+Users.hasMany(Posts);
+Posts.belongsTo(Users);
+Posts.hasMany(Comments);
+Comments.belongsTo(Posts);
+Comments.belongsTo(Users);
+
+//Create table structure for the database
+sequelize.sync({force: true})
